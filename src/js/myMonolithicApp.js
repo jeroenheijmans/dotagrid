72219fd48c101a083cbde1d60686987ab829d8cb
jeroenheijmans <jeroenheijmans@localhost>
2014-07-25 14:44:58 +0200

    Some styling, hero portraits, base version of the app.

--- src/js/myMonolithicApp.js
+++ src/js/myMonolithicApp.js
@@ -1,15 +1,17 @@
 ﻿var dotaGrid = (function (dg) {
 
-    dg.Hero = function() {
+    dg.Hero = function(row, col, hero) {
         var self = this;
-        self.id = ko.observable(128);
-        self.name = ko.observable("Sniper");
-    };
-    
+        self.key = hero.key;
+        self.name = hero.name;
+        self.row = ko.observable(row);
+        self.col = ko.observable(col);
+        self.imageSrc = "http://cdn.dota2.com/apps/dota2/images/heroes/" + self.key + "_vert.jpg";
+    };    
 
-    dg.Col = function() {
+    dg.Cell = function(hero) {
         var self = this;
-        self.hero = ko.observable(new dg.Hero());
+        self.hero = ko.observable(hero);
     };
 
     dg.Row = function(cols) {
@@ -22,21 +24,34 @@
         self.rowCount = ko.observable(7);
         self.colCount = ko.observable(25);
         
-        self.cellWidth = ko.computed(function() { return 800 / self.colCount(); });        
+        self.cellWidth = ko.observable(10);        
         self.cellWidthPx = ko.computed(function() { return self.cellWidth() + "px"; });
-        self.cellHeight = ko.computed(function() { return self.cellWidth() * 1.15; });
+        self.cellHeight = ko.observable(12);
         self.cellHeightPx = ko.computed(function() { return self.cellHeight() + "px"; });
         
         self.rows = ko.observableArray([]);
         
-        for (var r = 0; r < self.rowCount(); r++) {
-            var cols = [];
-            for (var c = 0; c < self.colCount(); c++) {
-                cols.push(new dg.Col());
-            }
-            var row = new dg.Row(cols);
-            self.rows.push(row);
+        self.resizeGrid = function(){
+            self.rows.removeAll();
+            self.cellWidth(960 / self.colCount());
+            self.cellHeight(self.cellWidth() * 272 / 235);
+                    
+            var i = 0;
+            
+            for (var r = 0; r < self.rowCount(); r++) {
+                var cols = [];
+                for (var c = 0; c < self.colCount(); c++) {
+                    var hero = !!tempHeroes[i] ? new dg.Hero(r, c, tempHeroes[i]) : null;
+                    i++;
+                    cols.push(new dg.Cell(hero));
+                }
+                var row = new dg.Row(cols);
+                self.rows.push(row);
+            }            
         }
+        
+        self.resizeGrid();
+        
     };
     
     return dg;
